af2d551ba5b7aefbf7fbb1e2db7b2620d957cfff
Adam Misiorny <adam.misiorny@gmail.com>
2015-02-27 10:37:57 +0100

    fix process interface for UnuseControllerWorker

--- src/Btn/BaseBundle/Assetic/Factory/Worker/UnuseControllerWorker.php
+++ src/Btn/BaseBundle/Assetic/Factory/Worker/UnuseControllerWorker.php
@@ -3,6 +3,7 @@
 namespace Btn\BaseBundle\Assetic\Factory\Worker;
 
 use Assetic\Asset\AssetInterface;
+use Assetic\Factory\AssetFactory;
 use Assetic\Factory\Worker\WorkerInterface;
 
 class UnuseControllerWorker implements WorkerInterface
@@ -10,7 +11,7 @@ class UnuseControllerWorker implements WorkerInterface
     /**
      * {@inheritdoc}
      */
-    public function process(AssetInterface $asset)
+    public function process(AssetInterface $asset, AssetFactory $factory)
     {
         $asset->setTargetPath(str_replace('_controller/', '', $asset->getTargetPath()));
 
