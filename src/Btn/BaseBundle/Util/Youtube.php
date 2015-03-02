0d32cca637923bc0469b2adee2c27d8b4fee9879
Adam Misiorny <adam.misiorny@gmail.com>
2015-03-02 17:06:26 +0100

    add Youtube Utility

---
+++ src/Btn/BaseBundle/Util/Youtube.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Btn\BaseBundle\Util;
+
+class Youtube
+{
+    const URL_REGEXP = '%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i';
+    const ID_REGEXP = '%^[^"&?/ ]{11}$%i';
+
+    /**
+     *
+     */
+    public static function getVideoIdFromUrl($url)
+    {
+        if (preg_match(self::URL_REGEXP, $url, $match)) {
+            return $match[1];
+        }
+    }
+
+    /**
+     *
+     */
+    public static function isValidUrl($url)
+    {
+        return preg_match(self::URL_REGEXP, $url) ? true : false;
+    }
+
+    /**
+     *
+     */
+    public static function isValidId($id) 
+    {
+        return preg_match(self::ID_REGEXP, $id) ? true : false;
+    }
+
+    /**
+     *
+     */
+    public static function getVideoId($input) 
+    {
+        if (self::isValidUrl($input)) {
+            return self::getVideoIdFromUrl($url);
+        } elseif (self::isValidId($input)) {
+            return $input;
+        }
+    }
+}
