547a899c2fd4458b79fdfaca6d0aba7be1dbbec7
Adam Misiorny <adam.misiorny@gmail.com>
2015-03-14 01:34:36 +0100

    allow to inject repository to entity provider

--- src/Btn/BaseBundle/Provider/EntityProviderInterface.php
+++ src/Btn/BaseBundle/Provider/EntityProviderInterface.php
@@ -2,12 +2,17 @@
 
 namespace Btn\BaseBundle\Provider;
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
+
 interface EntityProviderInterface
 {
+    public function setEntityManager(EntityManager $em);
     public function getEntityManager();
     public function getClass();
     public function getAlias();
     public function createQueryBuilder();
+    public function setRepository(EntityRepository $repository);
     public function getRepository();
     public function create();
     public function supports($class);
